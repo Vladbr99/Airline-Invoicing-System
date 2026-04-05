40890a722d69124dd898648cbdd43c4d077c4f09
Vladbr99 <b00162071@mytudublin.ie>
2026-04-05 11:29:28 +0100

    Migrations for database and registered admin for all models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, Flight, Invoice, InvoiceItem, UserProfile
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Flight)
+admin.site.register(Invoice)
+admin.site.register(InvoiceItem)
+admin.site.register(UserProfile)
